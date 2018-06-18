596ab7383096fb6cd8cccdde364ab1d543531cda
margo <margo.sockolova2010@yandex.ru>
2018-06-18 16:39:03 +0300

    add: Universe-singleton implementation

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("HEY");
+// Singleton
+let clz;
+
+function Universe() {
+  let size = 100;
+  if (typeof clz !== 'object') {
+    clz = this;
+    clz.getSize = function () {
+      return size;
+    };
+    clz.setSize = function (s) {
+      size = s;
+    };
+  }
+  return clz;
+}
+
+// Testing
+
+let a = new Universe();
+let b = new Universe();
+
+console.log(a === b); // должны получить true
+
+
+console.log(a.getSize(), b.getSize()); // 100, 100
+a.setSize(200);
+console.log(a.getSize(), b.getSize()); // 200, 200
