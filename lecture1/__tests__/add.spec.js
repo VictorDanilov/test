3090c415289c68e7c91552d61043b3fc9088200a
Viktor Danilov <wikslayer@gmail.com>
2019-04-20 19:36:24 +0300

    Add add and mul funs

---
+++ lecture1/__tests__/add.spec.js
@@ -0,0 +1,7 @@
+const add = require('../add')
+
+describe('add', () => {
+  it('should return 3', () => {
+    expect(add(1, 2)).toBe(3)
+  })
+})
---
+++ lecture1/__tests__/mul.spec.js
@@ -0,0 +1,7 @@
+const mul = require('../mul')
+
+describe('mul', () => {
+  it('should return 6', () => {
+    expect(mul(3, 2)).toBe(6)
+  })
+})
